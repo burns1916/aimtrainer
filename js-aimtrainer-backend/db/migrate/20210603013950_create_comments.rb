5dabec9c26bc7c0b512fbf5e24dd28331ed33360
Ryan Burns <ryankburns@gmail.com>
2021-06-02 18:57:26 -0700

    added table migrations

--- js-aimtrainer-backend/db/migrate/20210603013950_create_comments.rb
+++ js-aimtrainer-backend/db/migrate/20210603013950_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-
+      t.string :comment
       t.timestamps
     end
   end
--- js-aimtrainer-backend/db/migrate/20210603014038_create_high_scores.rb
+++ js-aimtrainer-backend/db/migrate/20210603014038_create_high_scores.rb
@@ -1,7 +1,7 @@
 class CreateHighScores < ActiveRecord::Migration[6.1]
   def change
     create_table :high_scores do |t|
-
+      t.integer :high_score
       t.timestamps
     end
   end
