dab4e3aaf6aebed4823e3bf2c390c49830c893e7
Ryan Burns <ryankburns@gmail.com>
2021-06-02 18:52:13 -0700

    added routes

--- js-aimtrainer-backend/config/routes.rb
+++ js-aimtrainer-backend/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :comments
+    resources :high_scores
+  end
+  resources :comments
+  resources :high_scores
 end
