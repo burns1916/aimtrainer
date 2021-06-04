97df81a121e489ded5e0a9dd8fcadf04903e41f2
Ryan Burns <ryankburns@gmail.com>
2021-06-03 17:30:33 -0700

    added validations

--- js-aimtrainer-backend/app/models/comment.rb
+++ js-aimtrainer-backend/app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
     belongs_to :user
+    validates :comment, presence: true
 end
--- js-aimtrainer-backend/app/models/user.rb
+++ js-aimtrainer-backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :comments
     has_one :high_score
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
