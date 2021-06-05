1f491fbf07bff44f637450a71d9a571170952551
Ryan Burns <ryankburns@gmail.com>
2021-06-05 11:10:54 -0700

    CRUD methods in High Scores Controller

--- js-aimtrainer-backend/app/controllers/high_score_controller.rb
+++ js-aimtrainer-backend/app/controllers/high_score_controller.rb
@@ -1,2 +1,20 @@
 class HighScoreController < ApplicationController
+    
+    def index
+        high_scores = HighScore.all
+        render json: high_scores, include: [:user]
+    end
+
+    def create
+        high_score = HighScore.new(high_score: params[:high_score], user_id: params[:user_id])
+        if high_score.save
+            render json: high_score, include: [:user]
+        else
+            render json: high_score.errors.full_messages
+    end
+
+    def update
+
+    end
+
 end
