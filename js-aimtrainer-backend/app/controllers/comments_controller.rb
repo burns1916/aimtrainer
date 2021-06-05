91caa130d81abbf702876ea71cbc1baab494e404
Ryan Burns <ryankburns@gmail.com>
2021-06-05 11:01:46 -0700

    CRUD methods in Comments controller

--- js-aimtrainer-backend/app/controllers/comments_controller.rb
+++ js-aimtrainer-backend/app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+
+    def index
+        comment = Comment.all
+        render json: comments, include: [:user]
+    end
+
+    def create
+        comment = Comment.new(comment: params[:comment], user_id: params[:user_id])
+        if comment.save
+            render json: comment, include: [:user]
+        else
+            render json: comment.errors.full_messages
+    end
+
+    def destroy
+        comment = Comment.find_by_id(params[:id])
+        comment.delete
+        render json: {commentId = comment.id}
+    end
 end
