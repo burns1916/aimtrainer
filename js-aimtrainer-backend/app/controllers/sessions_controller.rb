0fba8bc1737b30e29cdb5acd8a8b3b25a9e66639
Ryan Burns <ryankburns@gmail.com>
2021-06-05 12:07:00 -0700

    CRUD methods for User/Sessions Controller

--- js-aimtrainer-backend/app/controllers/sessions_controller.rb
+++ js-aimtrainer-backend/app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user
+            session[:user_id] = user.id
+            render json: user, only[:username, :id]
+        else
+            render json: { message: "User Not Found" }
+        end
+    end
+
+    def destroy
+        if session[:user_id]
+            reset_session
+    end
 end
