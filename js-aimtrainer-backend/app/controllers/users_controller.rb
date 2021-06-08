67f0874ec09095899c3d96646cbd634bce8c819c
Ryan Burns <ryankburns@gmail.com>
2021-06-07 17:07:41 -0700

    added CRUD methods to Users Controller

--- js-aimtrainer-backend/app/controllers/users_controller.rb
+++ js-aimtrainer-backend/app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
 
-    def new
+    def index
+        users = User.all
+        render json: users, only: [:id, :username]
+    end
 
+    def shwo
+        user = User.find_by_id(params[:id])
+        render json: user, only [:id, :username]
     end
 
     def create
+        user = User.new(user_params)
+        if uuser.save
+            session[:user_id] = user.id
+            render json: user, only: [:id, :username]
+        else
+            render json: user.errors.full_messages
+        end
+    end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
     end
     
 end
