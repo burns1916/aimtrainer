d33bc250e06cd304146687ddafac27d82cdd900e
Ryan Burns <ryankburns@gmail.com>
2021-06-09 20:53:09 -0700

    changed comment column name

---
+++ js-aimtrainer-backend/db/migrate/20210610035120_change_comment_column_name.rb
@@ -0,0 +1,5 @@
+class ChangeCommentColumnName < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :comments, :comment, :text
+  end
+end
--- js-aimtrainer-backend/db/schema.rb
+++ js-aimtrainer-backend/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_234415) do
+ActiveRecord::Schema.define(version: 2021_06_10_035120) do
 
   create_table "comments", force: :cascade do |t|
-    t.string "comment"
+    t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
